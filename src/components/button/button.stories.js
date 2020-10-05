71961f65177bc334724552d476e4a3ba5e6bf500
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-05 16:25:16 +0200

    Add share-button

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -2,9 +2,11 @@ import "./button.css";
 import buttonText from "./button-text.html";
 import buttonLogo from "./button-logo.html";
 import buttonPlay from "./button-play.html";
+import buttonShare from "./button-share.html";
 
 export default { title: "Components/Button" };
 
 export const withText = () => buttonText;
 export const withLogo = () => buttonLogo;
 export const playButton = () => buttonPlay;
+export const shareButton = () => buttonShare;
