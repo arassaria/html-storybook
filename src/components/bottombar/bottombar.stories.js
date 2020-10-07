46d7a0aecda90951f9ef7ced172ab392e5cf19f0
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-07 14:16:56 +0200

    Add Bottom-Navbar

---
+++ src/components/bottombar/bottombar.stories.js
@@ -0,0 +1,10 @@
+import "./bottombar.css";
+import buttonHome from "./button-home.html";
+import buttonSongs from "./button-songs.html";
+import buttonSettings from "./button-settings.html";
+
+export default { title: "Components/Bottombar" };
+
+export const btnHome = () => buttonHome;
+export const btnSongs = () => buttonSongs;
+export const btnSettings = () => buttonSettings;
---
+++ src/components/home-button/home.stories.js
@@ -0,0 +1,6 @@
+import "./home.css";
+import home from "./home.html";
+
+export default { title: "Components/Home-Button" };
+
+export const homecreation = () => home;
