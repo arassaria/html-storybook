9c494caef6ae8e2e7442b73e298f44e49239cd9a
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-08 17:43:36 +0200

    Add settings-page

--- src/components/bottomnav/bottomnav.stories.js
+++ src/components/bottomnav/bottomnav.stories.js
@@ -1,6 +1,6 @@
 import "./bottomnav.css";
 import bottomnav from "./bottomnav.html";
 
-export default { title: "Pages/Bottomnav" };
+export default { title: "Components/Bottomnav" };
 
 export const navbottom = () => bottomnav;
---
+++ src/pages/settings/settings.stories.js
@@ -0,0 +1,6 @@
+import "./settings.css";
+import settings from "./settings.html";
+
+export default { title: "Pages/settings" };
+
+export const Settings = () => settings;
