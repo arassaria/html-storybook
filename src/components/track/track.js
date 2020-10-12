a40d2159049a344f83223710bc21be4f76ea1969
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-12 13:51:09 +0200

    Modify track

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,12 +1,17 @@
 import playImage from "../../assets/play2.svg";
 import panikknopfImage from "../../assets/panikknopf.jpg";
+import "./track.css";
 
 export function createTrackElement() {
   const divElement = document.createElement("div"); // <div></div>
+  divElement.classList.add("track");
   //   divElement.innerText = "This is a track"; // <div>This is a track</div>
   const imgElement = document.createElement("img"); // <img>
+  imgElement.classList.add("band");
   imgElement.src = panikknopfImage; // <img src="../../assets/panikknopf.jpg">
   imgElement.alt = "panikknopf logo"; // <img src="../../assets/panikknopf.jpg" alt="panikknopf logo">
+
+  const div2Element = document.createElement("div"); // <div></div>
 
   const h3Element = document.createElement("h3"); // <h3></h3>
   h3Element.innerText = "Feuer"; // <h3>Feuer</h3>
@@ -15,14 +20,16 @@ export function createTrackElement() {
   spanElement.innerText = "Panikknopf"; // <span>Panikknopf</span>
 
   const btnElement = document.createElement("button"); // <button></button>
+  btnElement.classList.add("play-btn");
   const playElement = document.createElement("img"); // <img>
   playElement.src = playImage; //<img src="../../assets/play-icon.svg">
   playElement.alt = "play"; //<img src="../../assets/play.svg" alt="play">
   btnElement.append(playElement); //<button><img src="..." alt="..."></button>
 
+  div2Element.append(h3Element);
+  div2Element.append(spanElement);
   divElement.append(imgElement); // <div><img src="../../assets/panikknopf.jpg" alt="panikknopf logo"></div>
-  divElement.append(h3Element); // <div><h3>Feuer</h3></div>
-  divElement.append(spanElement); // <div><span>Panikknopf</span></div>
+  divElement.append(div2Element);
   divElement.append(btnElement); //<div><button></button></div>
   return divElement;
 }
