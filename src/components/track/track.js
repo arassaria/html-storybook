f34d9541227bd588a1a7b2b71d152fffc5531fd2
domko95 <dominique.kolbe1995@googlemail.com>
2020-10-12 17:01:23 +0200

    Modify style and functions

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,35 +1,36 @@
-import playImage from "../../assets/play2.svg";
-import panikknopfImage from "../../assets/panikknopf.jpg";
 import "./track.css";
+import playImage from "../../assets/play2.svg";
+import artistImage from "../../assets/panikknopf-1.jpg";
 
-export function createTrackElement() {
-  const divElement = document.createElement("div"); // <div></div>
-  divElement.classList.add("track");
+export function createTrackElement(title, artist) {
+  const trackElement = document.createElement("div"); // <div></div>
+  trackElement.className = "track";
   //   divElement.innerText = "This is a track"; // <div>This is a track</div>
-  const imgElement = document.createElement("img"); // <img>
-  imgElement.classList.add("band");
-  imgElement.src = panikknopfImage; // <img src="../../assets/panikknopf.jpg">
-  imgElement.alt = "panikknopf logo"; // <img src="../../assets/panikknopf.jpg" alt="panikknopf logo">
+  const trackCoverElement = document.createElement("img"); // <img>
+  trackCoverElement.className = "track__cover";
+  trackCoverElement.src = artistImage; // <img src="../../assets/panikknopf.jpg">
+  trackCoverElement.alt = `Image of ${artist}`; // <img src="../../assets/panikknopf.jpg" alt="panikknopf logo">
 
-  const div2Element = document.createElement("div"); // <div></div>
+  const trackInfoElement = document.createElement("div"); // <div></div>
+  trackInfoElement.className = "track__info";
 
-  const h3Element = document.createElement("h3"); // <h3></h3>
-  h3Element.innerText = "Feuer"; // <h3>Feuer</h3>
+  const titleElement = document.createElement("h3"); // <h3></h3>
+  titleElement.innerText = title; // <h3>Feuer</h3>
 
-  const spanElement = document.createElement("span"); // <span></span>
-  spanElement.innerText = "Panikknopf"; // <span>Panikknopf</span>
+  const artistElement = document.createElement("span"); // <span></span>
+  artistElement.innerText = artist; // <span>Panikknopf</span>
 
   const btnElement = document.createElement("button"); // <button></button>
-  btnElement.classList.add("play-btn");
+  btnElement.className = "play-btn";
+  btnElement.onclick = function () {
+    alert("Click!");
+  };
   const playElement = document.createElement("img"); // <img>
   playElement.src = playImage; //<img src="../../assets/play-icon.svg">
   playElement.alt = "play"; //<img src="../../assets/play.svg" alt="play">
   btnElement.append(playElement); //<button><img src="..." alt="..."></button>
 
-  div2Element.append(h3Element);
-  div2Element.append(spanElement);
-  divElement.append(imgElement); // <div><img src="../../assets/panikknopf.jpg" alt="panikknopf logo"></div>
-  divElement.append(div2Element);
-  divElement.append(btnElement); //<div><button></button></div>
-  return divElement;
+  trackInfoElement.append(titleElement, artistElement);
+  trackElement.append(trackCoverElement, trackInfoElement, btnElement); // <div><img src="../../assets/panikknopf.jpg" alt="panikknopf logo"></div>
+  return trackElement;
 }
--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -5,4 +5,20 @@ export default {
   parameters: { layout: "centered" },
 };
 
-export const feuer = () => createTrackElement();
+export const gothic = () => createTrackElement("Gothic", "Panikknopf");
+export const feuer = () => createTrackElement("Feuer", "Panikknopf");
+export const sagenAusMädchen = () =>
+  createTrackElement("Sagen aus Mädchen", "Panikknopf");
+export const wirSindDerKnopf = () =>
+  createTrackElement("Wir sind der Knopf", "Panikknopf");
+export const sexIstEineMacht = () =>
+  createTrackElement("Sex ist eine Macht", "Panikknopf");
+export const königinDerMaden = () =>
+  createTrackElement("Königin der Maden", "Panikknopf");
+export const bockAufSex = () =>
+  createTrackElement("Bock auf Sex", "Panikknopf");
+export const leichenbraut = () =>
+  createTrackElement("Leichenbraut", "Panikknopf");
+export const frei = () => createTrackElement("Frei", "Panikknopf");
+export const hoffnungAufSchmerz = () =>
+  createTrackElement("Hoffnung auf Schmerz", "Panikknopf");
